111f84698993e14873cbfe0e4678e86838062e80
amirhossain <amir.hossain@outlook.com.au>
2024-02-18 00:39:52 +1100

    Use vercel/ncc

--- dist/index.js
+++ dist/index.js
@@ -38448,9 +38448,9 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 
-  const response = fetch('https://api.example.com/data');
-  const json = response.json();
-  console.log(`${json}`)
+  const response =  fetch('https://api.example.com/data');
+  const json =  response.json();
+  console.log(json.JSON())
 } catch (error) {
   core.setFailed(error.message);
 }
